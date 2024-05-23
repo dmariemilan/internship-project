0af7da20170fca65be1b85bdb473d66c30588400
Donna M <milandm123@hotmail.com>
2024-05-22 21:23:01 -0500

    fresh start

--- app/application.py
+++ app/application.py
@@ -2,7 +2,6 @@ from pages.account_side_nav import AccountSideNav
 from pages.base_page import Page
 from pages.cart_page import CartPage
 from pages.header import Header
-from pages.help_page import HelpPage
 from pages.main_page import MainPage
 from pages.search_results_page import SearchResultsPage
 from pages.signin_page import SigninPage
@@ -19,7 +18,6 @@ class Application:
         self.base_page = Page(driver)
         self.cart_page = CartPage(driver)
         self.header = Header(driver)
-        self.help_page = HelpPage(driver)
         self.main_page = MainPage(driver)
         self.search_result_page = SearchResultsPage(driver)
         self.signin_page = SigninPage(driver)
